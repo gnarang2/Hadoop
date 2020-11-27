f54b09569963e1d7d1996f7be564ce1ee3f0d06f
unknown <gnarang2@illinois.edu>
2020-11-27 22:44:37 +0530

    Checking program.jar

---
+++ Executables/FileOperations.java
@@ -0,0 +1,223 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileOperations {
+
+    private String input;
+    private String output;
+    private String machineNumber;
+    private Long start;
+    private Long end;
+    private FileInputStream fis = null;
+    private BufferedInputStream bis = null;
+    private Long currentPointer;
+
+
+    public FileOperations(String fileIn, String fileOut, String machineID, Long start, Long end) {
+        this.input = fileIn;
+        this.output = fileOut;
+        this.start = start;
+        this.currentPointer = this.start;
+        this.end = end;
+        this.machineNumber = machineID;
+        openFile();
+    }
+
+    public boolean openFile(){
+        if(fis != null && bis != null){
+            return true;
+        }
+        File file = new File(this.input);
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+        bis = new BufferedInputStream(fis);
+        try {
+            bis.skip(this.start);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public ArrayList<Long> getDivisions(Integer numDivisions){
+        
+        ArrayList<Long> partitions = new ArrayList<>();
+        File file = new File(this.input);
+        Long length = file.length();
+        Long divisionLength = length/numDivisions;
+        Long currentLength = 0L;
+        byte[] temp = new byte[1];
+        partitions.add(0L);
+        Integer flag = 0;
+        Long offset = 0L;
+        try {
+            while (bis.read(temp) >= 1) {
+                currentLength += 1L;
+                offset += 1L;
+                if(currentLength == divisionLength){
+                    flag = 1;
+                }
+                String var = new String(temp);
+                if(flag == 1 && var.equalsIgnoreCase("\n")){
+                    flag = 0;
+                    currentLength = 0L;
+                    partitions.add(offset);
+                    partitions.add(offset+1L);
+                }
+            }
+        } catch (IOException e) {
+            return new ArrayList<Long>();
+        }
+        partitions.add(offset);
+
+        return partitions;
+
+    }
+
+    public String[] getFileData() {
+
+        Integer length = 20;
+        String[] data = new String[length];
+        if(!openFile()){
+            return data;
+        }
+        Integer currentLines = 0;
+        byte[] temp = new byte[1];
+        String var = new String("");
+        int n = 0;
+        while(length > 0 && this.currentPointer <= this.end){
+            try {
+                n = bis.read(temp);
+            } catch (IOException e) {
+                return new String[0];
+            }
+            if(n == -1){
+                break;
+            }
+            this.currentPointer += 1L;
+            String currentChar = new String(temp);
+            if(currentChar.equalsIgnoreCase("\n")){
+                data[currentLines] = var;//.substring(0,var.length()-1);
+                var = new String("");
+                currentLines += 1;
+                length -= 1;
+                continue;
+            }
+            System.out.println(var);
+            var += currentChar;
+        }
+
+        if(currentPointer == end){
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+            }
+        }
+
+        return Arrays.copyOfRange(data, 0, currentLines);
+        
+        // byte[] contents = new byte[0];
+        // File file = new File(this.input);
+        // FileInputStream fis;
+        // int bytesRead = 0;
+        // String[] data = new String[0];
+
+        // try {
+        //     fis = new FileInputStream(file);
+        // } catch (FileNotFoundException e) {
+        //     return data;
+        // }
+
+        // BufferedInputStream bis = new BufferedInputStream(fis);
+        // contents = new byte[end-start];
+        // try {
+        //     bis.skip((long)start);
+        //     bytesRead = bis.read(contents, start, end-start);
+        // } catch (IOException e) {
+        //     bytesRead = 0;
+        //     contents = new byte[0];
+        // }
+
+        // try {
+        //     fis.close();
+        //     bis.close();
+        // } catch (IOException e) {
+        //     bytesRead = 0;
+        //     contents = new byte[0];
+        // }
+        // if(contents.length == 0){
+        //     return data;
+        // }
+        // contents = Arrays.copyOf(contents, bytesRead);
+        // String temp = new String(contents);
+        // data = temp.split("\n");
+        // return data;
+    }
+
+    // public String convertBytesToString(byte[] data) {
+    //     return null;
+    // }
+
+    // public byte[] convertStringToBytes(String data) {
+    //     return null;
+    // }
+
+    public void createOutputFile(String fileName) {
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+        }
+    }
+
+    public void appendFileData(String data, String key){
+        String out = createPreMappleOutputFileName(key);
+        if(checkFileExists(out)){
+            createOutputFile(out);    
+        }
+
+        // System.out.println("Data appending is:" + data);
+        File file = new File(out);
+        FileWriter fr;
+        try {
+            fr = new FileWriter(file, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            br.write(data);
+    
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+        }
+ 
+    }
+
+    public boolean checkFileExists(String fileName){
+        File file = new File(fileName);
+        if (file.exists()) {
+            return true;
+        }
+        return false;
+        
+    }
+
+    public String createPreMappleOutputFileName(String key){
+        return String.join("_", this.output, this.machineNumber, key) + ".txt";
+    }
+
+
+
+
+}
---
+++ Executables/sub.java
@@ -0,0 +1,91 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class sub {
+
+    public static void mapple(FileOperations myObj){
+        String[] temp = new String[0];
+        while(true){
+            temp = myObj.getFileData();
+            if(temp.length == 0){
+                break;
+            }
+            for(String pair: temp){
+                myObj.appendFileData(new String(pair + " 1\n"), pair);
+            }
+        }
+    }
+
+    public static String getDivisions(Integer numDivisions, String fileName){
+        
+        ArrayList<Long> partitions = new ArrayList<>();
+        
+        FileInputStream fis;
+        BufferedInputStream bis;
+        File file = new File(fileName);
+        try {
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+        } catch (FileNotFoundException e) {
+            return new String();
+        }
+        
+        Long length = file.length();
+        Long divisionLength = length/numDivisions;
+        Long currentLength = 0L;
+        byte[] temp = new byte[1];
+        partitions.add(0L);
+        Integer flag = 0;
+        Long offset = 0L;
+        try {
+            while (bis.read(temp) >= 1) {
+                currentLength += 1L;
+                offset += 1L;
+                if(currentLength.equals(divisionLength)){
+                    flag = 1;
+                }
+                String var = new String(temp);
+                if(flag == 1 && var.equalsIgnoreCase("\n")){
+                    flag = 0;
+                    currentLength = 0L;
+                    partitions.add(offset);
+                    partitions.add(offset+1L);
+                }
+            }
+        } catch (IOException e) {
+            return new String();
+        }
+        partitions.add(offset);
+
+        String response = "";
+        for(int i = 0; i < partitions.size(); i++){
+            response += Long.toString(partitions.get(i)) + ":"; 
+        }
+
+        try {
+            fis.close();
+            bis.close();
+        } catch (IOException e) {
+        }
+        
+        return response.substring(0, response.length()-1);
+
+    }
+
+    public static void main(String[] args){
+        
+        // String inputFile = args[0];
+        // String outputFile = args[1];
+        // String machineNumber = args[2];
+        // Long startOff = Long.parseLong(args[3]);
+        // Long endOff = Long.parseLong(args[4]);
+        // FileOperations obj = new FileOperations(inputFile, outputFile, machineNumber, startOff, endOff);
+        // mapple(obj);
+        System.out.println(getDivisions(4, "test.txt"));
+        
+    }
+}
